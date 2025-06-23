0eef8683f5ffb511c01b9618de66d9dcfe594888
SAI BHARADWAJ <saibharadwajkinthali060305@gmail.com>
2025-06-23 13:57:54 +0530

    this is my first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='scrapper',
+    version='0.0.1',
+    author='saikb',
+    author_email='saibharadwajkinthali060305@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
